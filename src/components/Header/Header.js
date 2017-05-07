9d22fb87d707f6092f1437a4ea204529ba9c5591
korrybass <korry@penheadcreative.com>
2017-05-07 19:40:35 -0400

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './header.scss';
+import logo from '../../assets/img//penhead_logo4.png'
 
 
 class Header extends Component {
   render() {
     return (
       <div className="header flex-row between">
-        <div className="logo"> Logo </div>
+        <div className="nav-logo"> <img className="logo" src={logo} alt='' /> </div>
         <div className='nav-container'>
           <ul className="flex-row ">
             <li>Link</li>
